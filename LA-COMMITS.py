5b9dd4aefd35aea22a704275d378748bbaaaa171
Louis Popi <theh2o64@gmail.com>
2017-02-25 11:05:06 +0100

    la-commits: Move everything to funct

--- LA-COMMITS.py
+++ LA-COMMITS.py
@@ -1,7 +1,5 @@
 import os
 
-curl = 'curl -s --request GET https://review.lineageos.org/changes/?q=status:open+owner:"theh2o64@gmail.com"+project:LineageOS/'
-
 def filter(liste):
 	# Filter data
 	tmp_liste = []
@@ -29,23 +27,30 @@ def gather(remotes):
 	return (project_out,topic_out,numbers_out,subject_out)
 
 # Filter and un-split
-target = ["android_kernel_cyanogen_msm8916","android_device_yu_tomato","android_device_yu_lettuce","android_device_yu_jalebi","android_device_cyanogen_msm8916-common"]
-project_global,topic_global,numbers_global,subject_global = gather(target)
-project = [0]*len(target)
-topic = [0]*len(target)
-numbers = [0]*len(target)
-subject = [0]*len(target)
-for k in range(len(target)):
-	project[k] = project_global[k]
-	topic[k] = filter(topic_global[k])
-	numbers[k] = filter(numbers_global[k])
-	subject[k] = filter(subject_global[k])
+def split(project_global, topic_global, numbers_global, subject_global):
+	project_local = [0]*len(target)
+	topic_local = [0]*len(target)
+	numbers_local = [0]*len(target)
+	subject_local = [0]*len(target)
+	for k in range(len(target)):
+		project_local[k] = project_global[k]
+		topic_local[k] = filter(topic_global[k])
+		numbers_local[k] = filter(numbers_global[k])
+		subject_local[k] = filter(subject_global[k])
+	return (project_local,topic_local,numbers_local,subject_local)
 
 # Print data
-for k in range(len(target)):
-	for j in range(len(numbers[k])):
-		print('Change number : ' + numbers[k][j])
-		print('Project : ' + project[k])
-		print('Number : ' + numbers[k][j])
-		print('Subject : ' + subject[k][j])
-		print('Topic : ' + topic[k][j])
+def results():
+	curl = 'curl -s --request GET https://review.lineageos.org/changes/?q=status:open+owner:"theh2o64@gmail.com"+project:LineageOS/'
+	target = ["android_kernel_cyanogen_msm8916","android_device_yu_tomato","android_device_yu_lettuce","android_device_yu_jalebi","android_device_cyanogen_msm8916-common"]
+	project_global,topic_global,numbers_global,subject_global = gather(target)
+	project,topic,numbers,subject = split(project_global, topic_global, numbers_global, subject_global)
+	for k in range(len(target)):
+		for j in range(len(numbers[k])):
+			print('#####################')
+			print('Project : ' + project[k])
+			print('Change number : ' + numbers[k][j])
+			print('Subject : ' + subject[k][j])
+			if not topic[k][j] == 'null': print('Topic : ' + topic[k][j])
+	for l in range(len(target)):
+		print 'Number of commits for ' + project[l] + ' = ' + str(len(numbers[l]))
