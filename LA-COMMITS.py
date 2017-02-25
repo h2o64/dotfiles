cbf1c2d8d2a8f4b114353c564a00f8c7bd8f50ea
Louis Popi <theh2o64@gmail.com>
2017-02-25 18:40:41 +0100

    la-commits: Add picks() to draw a dynamic pickable list in script form

--- LA-COMMITS.py
+++ LA-COMMITS.py
@@ -1,6 +1,10 @@
 from __future__ import division
 import datetime
 import os
+import copy
+
+curl = 'curl -s --request GET https://review.lineageos.org/changes/?q=status:open+owner:"theh2o64@gmail.com"+project:LineageOS/'
+target = ["android_kernel_cyanogen_msm8916","android_device_yu_tomato","android_device_yu_lettuce","android_device_yu_jalebi","android_device_cyanogen_msm8916-common"]
 
 # Taken from http://stackoverflow.com/questions/8777753/converting-datetime-date-to-utc-timestamp-in-python/8778548#8778548
 # and http://stackoverflow.com/questions/19068269/how-to-convert-a-string-date-into-datetime-format-in-python
@@ -10,6 +14,19 @@ def totimestamp(dt, epoch=datetime.datetime(1970,1,1)):
 
 def utctotimestamp(string):
 	return totimestamp(datetime.datetime.strptime(string[:len(string)-4], "%Y-%m-%d %H:%M:%S.%f"))
+
+def quicksort(t):
+	if t == []: return []
+	else:
+		pivot = t[0]
+		t1 = []
+		t2 = []
+		for x in t[1:]:
+			if x<pivot:
+				t1.append(x)
+			else:
+				t2.append(x)
+	return quicksort(t1)+[pivot]+quicksort(t2)
 
 def filter(liste):
 	# Filter data
@@ -58,8 +75,6 @@ def split(project_global, topic_global, numbers_global, subject_global,updated_g
 
 # Print data
 def results():
-	curl = 'curl -s --request GET https://review.lineageos.org/changes/?q=status:open+owner:"theh2o64@gmail.com"+project:LineageOS/'
-	target = ["android_kernel_cyanogen_msm8916","android_device_yu_tomato","android_device_yu_lettuce","android_device_yu_jalebi","android_device_cyanogen_msm8916-common"]
 	project_global,topic_global,numbers_global,subject_global,updated_global = gather(target)
 	project,topic,numbers,subject,updated = split(project_global, topic_global, numbers_global, subject_global,updated_global)
 	for k in range(len(target)):
@@ -73,3 +88,50 @@ def results():
 			if not topic[k][j] == 'null': print('Topic : ' + topic[k][j])
 	for l in range(len(target)):
 		print 'Number of commits for ' + project[l] + ' = ' + str(len(numbers[l]))
+
+# Filtering to show last updated last
+def order(topic_fil,numbers_fil,subject_fil,updated_fil):
+	new_time = copy.deepcopy(updated_fil)
+	dummy_copy = copy.deepcopy(updated_fil)
+	new_time_sorted = copy.deepcopy(updated_fil)
+	# Keep UTC alphabetic time sorted
+	for k in range(len(target)): new_time_sorted[k] = quicksort(updated_fil[k])
+	# Convert the timestamp UTC to numerical
+	for k in range(len(target)):
+		for j in range(len(numbers[k])):
+			new_time[k][j] = (utctotimestamp(dummy_copy[k][j]),k,j)
+	# Sort numerical timestamp
+	for k in range(len(target)): new_time[k] = copy.deepcopy(quicksort(new_time[k]))
+	# print str(new_time)
+	topic_new = copy.deepcopy(topic_fil)
+	numbers_new = copy.deepcopy(numbers_fil)
+	subject_new = copy.deepcopy(subject_fil)
+	time_swap(topic_fil, topic_new, new_time, new_time_sorted, dummy_copy)
+	time_swap(numbers_fil, numbers_new, new_time, new_time_sorted, dummy_copy)
+	time_swap(subject_fil, subject_new, new_time, new_time_sorted, dummy_copy)
+	return topic_new,numbers_new,subject_new,new_time_sorted
+
+def time_swap(old, new, time_ref, new_time_bis, old_time):
+	for k in range(len(target)):
+		for j in range(len(numbers[k])):
+			# print 'old time = ' + str(old_time[k][j]) + ' | new time = ' + str(new_time_bis[k][j])
+			new = old[time_ref[k][j][1]][time_ref[k][j][2]]
+
+# Cherry picks
+def picks():
+	project_global,topic_global,numbers_global,subject_global,updated_global = gather(target)
+	project,topic,numbers,subject,updated = split(project_global, topic_global, numbers_global, subject_global,updated_global)
+	topic,numbers,subject,updated = order(topic,numbers,subject,updated)
+	ret_topic = [0]*len(target)
+	ret_numbers = [0]*len(target)
+	ret_subject = [0]*len(target)
+	for k in range(len(target)):
+		# Reverse everything to show min first
+		# TODO: Fix this in quicksort
+		ret_topic[k] = copy.deepcopy(topic[k][::-1])
+		ret_numbers[k] = copy.deepcopy(numbers[k][::-1])
+		ret_subject[k] = copy.deepcopy(subject[k][::-1])
+		for j in range(len(numbers[k])):
+			print 'gerrit-cherry-pick ssh://h2o64@review.lineageos.org:29418/LineageOS/' + project[k] + ' ' + ret_numbers[k][j] + ' # ' + ret_subject[k][j] + ' - ' + updated[k][j]
+	for l in range(len(target)):
+		print '# Number of commits for ' + project[l] + ' = ' + str(len(ret_numbers[l]))
