e0c6a8c798c4dd02ba16061e139ca8b661b95773
Louis Popi <theh2o64@gmail.com>
2017-03-01 09:29:34 +0100

    la-commits: Add extra commits ability

--- LA-COMMITS.py
+++ LA-COMMITS.py
@@ -5,10 +5,19 @@ import copy
 
 curl = 'curl -s --request GET https://review.lineageos.org/changes/?q=status:open+owner:"theh2o64@gmail.com"+project:LineageOS/'
 target = ["android_kernel_cyanogen_msm8916","android_device_yu_tomato","android_device_yu_lettuce","android_device_yu_jalebi","android_device_cyanogen_msm8916-common"]
+
+# Blacklisting
 commit_blacklist = [[''],[''],['163364'],[''],['']]
 word_blacklist = ['','','','','']
-id_black_per_rom = ['162848', '162848']
-rom_black_per_rom = ['DU', 'SLIM'] # Easier than tuplet
+id_black_per_rom = ['162848', '162848','163078','163078']
+rom_black_per_rom = ['DU', 'SLIM','DU', 'SLIM'] # Easier than tuplet
+
+# Extra commits
+github_extra = ['ashwinr64/proprietary_vendor_yu/commit/9ccd8fa3b1c7de619931722fd423e3a2051c9f69']
+github_extra_branch = ['cm-14.1']
+gerrit_extra = ['163803']
+
+# Global variables
 repos_count = len(target)
 commits_count = [0]*repos_count
 
@@ -142,7 +151,7 @@ def picks():
 	blacklist_word_count = [0]*repos_count
 	word_blacklist_bool = True
 	print 'CURRENT_DIR=$1'
-	print 'CURRENT_DIR_NAME=basename $CURRENT_DIR'
+	print 'CURRENT_DIR_NAME=$(basename $CURRENT_DIR)'
 	for k in range(repos_count):
 		# Reverse everything to show min first
 		# TODO: Fix this in quicksort
@@ -154,21 +163,61 @@ def picks():
 			if word_blacklist[k] not in ret_subject[k][j]:
 				blacklist_word_count[k] += - 1
 				word_blacklist_bool = False
-			if ret_numbers[k][j] not in commit_blacklist[k] or word_blacklist_bool:
+			if ret_numbers[k][j] not in commit_blacklist[k]: # or word_blacklist_bool:
 				for i in range(len(id_black_per_rom)):
 					if ret_numbers[k][j] == id_black_per_rom[i]: banned_rom_ind.append(i)
 				for m in banned_rom_ind:
 					tmp += '$CURRENT_DIR_NAME != "' + rom_black_per_rom[m] + '" '
 					if m+1 < len(banned_rom_ind): tmp += ' || '
-				if ret_numbers[k][j] in id_black_per_rom:
-					print tmp + ' ]; then'
-					tmp = 'if [ '
+				if ret_numbers[k][j] in id_black_per_rom: print tmp + ' ]; then'
+				tmp = 'if [ '
 				print git_fetch + project[k] + ' refs/changes/' + ret_numbers[k][j][-2] + ret_numbers[k][j][-1] + '/' + ret_numbers[k][j] + '/' + get_patchset(ret_numbers[k][j]) +' && git cherry-pick FETCH_HEAD # ' + ret_subject[k][j] + ' - ' + updated[k][j]
 				if ret_numbers[k][j] in id_black_per_rom: print 'fi'
+			# Reset
 			word_blacklist_bool = True
+			banned_rom_ind = []
 		print 'cd $CURRENT_DIR'
+	cherry = ''
+	suffix = ''
+	remote = ''
+	is_proprietary = False
+	if not len(github_extra) == 0:
+		for ind in range(len(github_extra)):
+			commit = github_extra[ind]
+			if 'proprietary_vendor' in commit: is_proprietary = True
+			for i in range(len(commit)):
+				suffix = copy.deepcopy(commit[i:])
+				if suffix.startswith('android_') or suffix.startswith('proprietary_vendor_'):
+					suffix = copy.deepcopy(suffix[:len(suffix)-8-40])
+					remote = suffix
+					if is_proprietary: print 'cd ' + (suffix.replace('proprietary_','')).replace('_','/')
+					else:  print 'cd ' + (suffix.replace('android_','')).replace('_','/')
+					print 'git fetch ' + remote +  ' ' + github_extra_branch[ind]
+					print 'git cherry-pick ' + commit[-40:]
+					print 'cd $CURRENT_DIR'
+					# Reset
+					cherry = ''
+					suffix = ''
+					remote = ''
+					break
+	commit_details = []
+	if not len(gerrit_extra) == 0:
+		for m in gerrit_extra:
+			tmp = os.popen('ssh -p 29418 h2o64@review.lineageos.org gerrit query change:' + m + ' | grep "project: "').read() # Project
+			commit_details.append(((tmp.replace('project: ', '')).replace('\n','')).replace(' ',''))
+			tmp = os.popen('ssh -p 29418 h2o64@review.lineageos.org gerrit query change:' + m + ' --current-patch-set | grep "number: " | grep -v "' + m + '"').read() # Patchset
+			commit_details.append(((tmp.replace('number: ', '')).replace('\n','')).replace(' ',''))
+			tmp = os.popen('ssh -p 29418 h2o64@review.lineageos.org gerrit query change:' + m + ' | grep "commitMessage: "').read() # Subject
+			commit_details.append((tmp.replace('commitMessage: ', '')).replace('\n',''))
+			tmp = os.popen('ssh -p 29418 h2o64@review.lineageos.org gerrit query change:' + m + ' | grep "lastUpdated: "').read() # Updated
+			commit_details.append((tmp.replace('lastUpdated: ', '')).replace('\n',''))
+			print 'cd $CURRENT_DIR' + commit_details[0].replace('LineageOS/android', '').replace('_','/')
+			print git_fetch + commit_details[0] + ' refs/changes/' + m[-2] + m[-1] + '/' + m + '/' + commit_details[1] +' && git cherry-pick FETCH_HEAD #' + commit_details[2] + ' - ' + commit_details[3]
+			print 'cd $CURRENT_DIR'
+			commit_details = [] # Reset
 	for l in range(repos_count):
 		print '# Number of commits for ' + project[l] + ' = ' + str(len(ret_numbers[l]) - len(commit_blacklist[l]) + blacklist_word_count[l])
+	print '# Number of extra commits  = ' + str(len(github_extra) + len(gerrit_extra))
 
 if __name__ == '__main__':
     picks()
