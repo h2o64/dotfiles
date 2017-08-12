4f33d4791df0e1e094856e186da9587a11671c4e
Louis Popi <theh2o64@gmail.com>
2017-08-12 09:43:32 +0200

    la: Huge refractor
    * Make it stronger, smaller, faster, cleaner

--- LA-COMMITS.py
+++ LA-COMMITS.py
@@ -3,19 +3,15 @@ import datetime
 import os
 import copy
 
-curl = 'ssh -p 29418 h2o64@review.lineageos.org gerrit query --current-patch-set status:open owner:"theh2o64@gmail.com" | egrep "project:|number:|subject:|lastUpdated:|ref:"'
 target = ["LineageOS/android_device_cyanogen_msm8916-common",
 "LineageOS/android_device_yu_tomato",
 "LineageOS/android_device_yu_lettuce",
-"LineageOS/android_device_yu_jalebi"]
-target_open = ["LineageOS/android_device_xiaomi_msm8996-common",
+"LineageOS/android_device_yu_jalebi",
+"LineageOS/android_device_xiaomi_msm8996-common",
 "LineageOS/android_device_xiaomi_gemini",
 "LineageOS/android_kernel_xiaomi_msm8996"]
-commit_blacklist = ['165010','177854','178211']
-commit_blacklist += ['167063','167064','172016','171943','171614','167062','177764'] # xiaomi WIP srgb and dt2w
-commit_blacklist += ['178189','181121','181132'] # xiaomi NFC
-gerrit_extra = ['180256','180257','180255','181963']
-gerrit_extra += ['182778','183078','183079','183080','183081','182980','182981']
+commit_blacklist = ['']
+gerrit_extra = ['']
 github_extra = []
 #sumbit_command = 'gerrit review --code-review +1'
 sumbit_command = ''
@@ -46,58 +42,57 @@ def quicksort(t):
 				t2.append(x)
 	return quicksort(t1)+[pivot]+quicksort(t2)
 
+def curlRepo(name):
+			curl = 'ssh -p 29418 h2o64@review.lineageos.org gerrit query --current-patch-set status:open ' + name + ' | egrep "project:|number:|subject:|lastUpdated:|ref:"'
+			return os.popen(curl).readlines()
+
+def makeCommitList(data,mod):
+	commit_list = []
+	n = len(data)
+	i = 0
+	buf = []
+	# Error check
+	if n%mod != 0:
+		print "Error in data lenght"
+		return
+	while i != n:
+		if i%mod == 0:
+			commit_list.append(tuple(buf))
+			buf = []
+		buf.append(data[i][:-1])
+		i += 1
+	return commit_list[1:]+[tuple(buf)]
+
 def gather():
 		# Download data
-		data = os.popen(curl).read()
-		# Download data for status:open repos
-		if target_open != []:
-			for repo in target_open:
-				curl_open = 'ssh -p 29418 h2o64@review.lineageos.org gerrit query --current-patch-set status:open project:' + repo + ' | egrep "project:|number:|subject:|lastUpdated:|ref:"'
-				data += os.popen(curl_open).read()
+		data = []
+		# Download data
+		for repo in target: data += curlRepo('project:' + repo)
+		# Get personnal misc commits
+		data += curlRepo('owner:"theh2o64@gmail.com"')
+		# Download extra commits
 		if gerrit_extra != ['']:
-			for extra in gerrit_extra:
-				curl_extra = 'ssh -p 29418 h2o64@review.lineageos.org gerrit query --current-patch-set status:open change:' + extra + ' | egrep "project:|number:|subject:|lastUpdated:|ref:"'
-				data += os.popen(curl_extra).read()
-		# Filter data
-		cursor = 0
-		new_data = []
-		for i in range(len(data)):
-			if data[cursor:i].endswith('\n'):
-				new_data.append(data[cursor:i-1])
-				cursor = i
-			if i == len(data) - 1: new_data.append(data[cursor:i])
+			for extra in gerrit_extra: data += curlRepo('change:' + extra)
 		# Make a commit struct
-		commits_list = []
-		j = 0
-		while j < len(new_data):
-			# Project
-			# Number
-			# Subject
-			# lastUpdated
-			# patchset (not used)
-			# ref
-			commits_list.append((new_data[j],new_data[j+1],new_data[j+2],new_data[j+3],new_data[j+4],new_data[j+5]))
-			j += 6
+		commits_list = makeCommitList(data,6)
 		# Remove unwanted strings
+		buf = []
 		for k in range(len(commits_list)):
+			number = commits_list[k][1].replace("  number: ", "")
+			if number in commit_blacklist: continue # Apply a blacklist
 			project = commits_list[k][0].replace("  project: ", "")
 			updated = commits_list[k][3].replace("  lastUpdated: ", "")
-			number = commits_list[k][1].replace("  number: ", "")
 			subject = commits_list[k][2].replace("  subject: ", "")
 			patchset = commits_list[k][4].replace("    number: ", "")
 			ref = commits_list[k][5].replace("    ref: ", "")
-			commits_list[k] = (project,updated,number,subject,patchset,ref) # Add lastUpdated in higher prio
-		# Only take authorized repos
-		commits_edited = []
-		for l in commits_list:
-			if ((l[0] in target+target_open) and (l[2] not in commit_blacklist)) or l[2] in gerrit_extra: commits_edited.append(l)
+			buf.append((project,updated,number,subject,patchset,ref)) # Add lastUpdated in higher prio
 		# Convert timestamps to real time
 		time_list = []
-		for m in commits_edited:
+		for m in buf:
 			commit_time = utctotimestamp(m[1])
 			if commit_time > min_date : time_list.append((m[0],commit_time,int(m[2]),m[3],m[4],m[5]))
 		# Sort everything
-		time_list = quicksort(time_list)
+		time_list = quicksort(list(set(time_list)))
 		return time_list
 
 
@@ -121,51 +116,30 @@ def gather_github():
 # Print 'cd' a smart way
 def cd_print(cd_to_print,old_cd):
 	if not old_cd == cd_to_print:
+		# Print CD and checks
+		if old_cd == '':	print 'if [ -d "$CURRENT_DIR/' + cd_to_print + '" ]; then\n  cd $CURRENT_DIR/' + cd_to_print
+		else: print 'fi\nif [ -d "$CURRENT_DIR/' + cd_to_print + '" ]; then\n  cd $CURRENT_DIR/' + cd_to_print
 		old_cd = cd_to_print
-		print 'cd $CURRENT_DIR/' + cd_to_print
 	return old_cd
 
-# LA-Only ?
-def isLA_ONLY(project_name,old_p,old_b):
-	la_bool = True
-	for allowed in target:
-		if project_name in allowed: la_bool = False
-	if project_name in "LineageOS/android_bionic": la_bool = False
-	if 'android_device_yu_' in project_name: la_bool = False
-	if la_bool and project_name != old_p and not old_b: print 'if [ $CURRENT_DIR_NAME == "LA" ]; then'
-	if not la_bool and project_name != old_p and old_b: print 'fi'
-	old_p = project_name
-	old_b = la_bool
-	return (old_p,old_b)
-
+# Print everything
 def picks():
 	gerrit_list = gather()
 	if github_extra != ['']: github_list = gather_github()
 	old_cd = ''
-	old_project = ''
-	old_bool = False
 	print 'CURRENT_DIR=$1'
-	print 'CURRENT_DIR_NAME=$(basename $CURRENT_DIR)'
 	if sumbit_command != '':
 		for gerrit_c in gerrit_list:
-			print "ssh -p 29418 h2o64@review.lineageos.org " + sumbit_command + " " + str(gerrit_c[2]) + "," + gerrit_c[4] + ' # ' + gerrit_c[5]
+			print "  ssh -p 29418 h2o64@review.lineageos.org " + sumbit_command + " " + str(gerrit_c[2]) + "," + gerrit_c[4] + ' # ' + gerrit_c[5]
 	else:
 		for gerrit_c in gerrit_list:
-			old_project,old_bool = isLA_ONLY(gerrit_c[0],old_project,old_bool)
 			old_cd = cd_print((gerrit_c[0].replace('LineageOS/android_','')).replace('_','/'),old_cd)
-			print "git fetch ssh://h2o64@review.lineageos.org:29418/" + gerrit_c[0] + " " + gerrit_c[5] + " && git cherry-pick FETCH_HEAD # " + gerrit_c[3]
-			if gerrit_c == gerrit_list[-1] and old_bool:
-				print 'fi'
-				old_bool = False
+			print "  git fetch ssh://h2o64@review.lineageos.org:29418/" + gerrit_c[0] + " " + gerrit_c[5] + " && git cherry-pick FETCH_HEAD # " + gerrit_c[3]
 	if github_extra != ['']:
 		for github_c in github_list:
-			#old_project,old_bool = isLA_ONLY(github_c[0].replace('/',''),old_project,old_bool)
 			old_cd = cd_print((github_c[0].replace('android_','').replace('proprietary_','').replace('_','/') + "\ngit fetch " + github_c[1] + ' ' + github_c[3]),old_cd)
-			print 'git cherry-pick ' + github_c[2]
-			#if github_c == github_list[-1] and old_bool :
-			#	print 'fi'
-			#	old_bool = False
-	print 'cd $CURRENT_DIR'
+			print '  git cherry-pick ' + github_c[2]
+	print 'fi\ncd $CURRENT_DIR'
 
 if __name__ == '__main__':
     picks()
